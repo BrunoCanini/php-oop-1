e7ae540546107c1841d419ccf638724164ebd3d4
Bruno Canini <brunocanini1994@gmail.com>
2023-06-20 18:59:07 +0200

    doppia classe

--- index.php
+++ index.php
@@ -1,25 +1,40 @@
 <?php 
 
+class Indirizzo {
+    public $strada;
+    public $città;
+    public $codicePostale;
+
+    function __construct($_strada, $_città, $_codicePostale)
+    {
+        $this->strada = $_strada;
+        $this->città = $_città;
+        $this->codicePostale = $_codicePostale;
+    }
+}
+
 class Movie {
     public $nome;
     public $genere;
     public $lingua;
+    public $indirizzo;
 
-    function __construct($_nome, $_genere, $_lingua)
+    function __construct($_nome, $_genere, $_lingua, Indirizzo $indirizzo)
     {
         $this->nome = $_nome;
         $this->genere = $_genere;
         $this->lingua = $_lingua;
+        $this->indirizzo = $indirizzo;
     }
 
     public function getInfo(){
-        return  $this->nome . " " . $this->genere . " " . $this->lingua ;
+        return  $this->nome . " " . $this->genere . " " . $this->lingua . "(" .  $this->indirizzo->strada . ", " . $this->indirizzo->città . ", " . $this->indirizzo->codicePostale . ")" ;
     }
 }
 
-$batman = new Movie("Batman Il Cavaliere Oscuro", "Azione", "IT");
+$batman = new Movie("Batman Il Cavaliere Oscuro", "Azione", "IT", new Indirizzo("via laggiù", "hollywood", 90028 ));
 
-$diablo = new Movie("Diablo 4", "Splatter", "EN");
+$diablo = new Movie("Diablo 4", "Splatter", "EN", new Indirizzo("via quaggiù", "hollywood", 90028 ));
 
 ?>
 
